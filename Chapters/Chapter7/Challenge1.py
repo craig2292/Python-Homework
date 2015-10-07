e9877a05d0ff22f094843cfabaa046c676b166d5
craig2292 <craig2292@gmail.com>
2015-10-07 13:09:44 -0400

    Update Challenge1.py

--- Chapters/Chapter7/Challenge1.py
+++ Chapters/Chapter7/Challenge1.py
@@ -3,11 +3,9 @@ from arcpy import env
 env.workspace = "P:/Fall2015/Python/Python Scipting for ArcGIS/Data/Exercise07"
 
 fc = "airports.shp"
-airport = arcpy.CreateUniqueName("Results/buffer01.shp")
-seaplane = arcpy.CreateUniqueName("Results/buffer02.shp")
-
-arcpy.Buffer_analysis(fc, airport, "15000 METERS")
-arcpy.Buffer_analysis(fc, seaplane, "7500 METERS")
-print fc
+arcpy.Select_analysis(fc, "Results/airports_sel.shp", '"FEATURE" = \'Airport\'')
+arcpy.Buffer_analysis("Results/airports_sel.shp", "Results/airport_buffer.shp", "15000 METERS")
+arcpy.Select_analysis(fc, "Results/seaplane_sel.shp", '"FEATURE" = \'Seaplane Base\'')
+arcpy.Buffer_analysis("Results/seaplane_sel.shp", "Results/seaplane_buffer.shp", "7500 METERS")
 
 
