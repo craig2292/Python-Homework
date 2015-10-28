5200dd2d75945d53827259c77cb7cfff55c79ab5
craig2292 <craig2292@gmail.com>
2015-10-28 16:41:50 -0400

    Update Challenge1CallingScript.py

--- Chapters/Chapter12/Challenge1CallingScript.py
+++ Chapters/Chapter12/Challenge1CallingScript.py
@@ -1,4 +1,4 @@
 import arcpy
-import mycount
+import Challenge1MyCount as mycount
 table = "P:/Fall2015/Python/Python Scipting for ArcGIS/Data/Exercise12/streets.shp"
 print mycount.countstringfields (table)
