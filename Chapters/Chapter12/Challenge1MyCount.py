da001581e5ca62c18f874bb8de0b420b50bdb227
craig2292 <craig2292@gmail.com>
2015-10-28 16:39:25 -0400

    Update Challenge1MyCount.py

--- Chapters/Chapter12/Challenge1MyCount.py
+++ Chapters/Chapter12/Challenge1MyCount.py
@@ -1,6 +1,11 @@
 import arcpy
-import os
+from arcpy import env
+env.workspace = "P:/Fall2015/Python/Python Scipting for ArcGIS/Data/Exercise12"
+
 def countstringfields(table):
+    """ This function determines the number of fields of type string in an input feature class.
+    """
+    
     fields = arcpy.ListFields (table)
     i = 0
     for field in fields:
